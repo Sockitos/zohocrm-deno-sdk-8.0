a7ad1469471651f85838b22b71e502a3f5245425
João Nogueira <jr_nogueira1@msn.com>
2025-09-05 18:31:53 +0100

    Fix logger

--- routes/logger/sdk_logger.ts
+++ routes/logger/sdk_logger.ts
@@ -1,43 +1,16 @@
-import {Levels, Logger} from './logger.ts'
-import * as winston from 'winston';
+import { Logger } from "./logger.ts";
 
 /**
  * The class to initialize the SDK logger.
  */
 export class SDKLogger {
-    private constructor(loggerInstance: Logger) {
-        let filePath = loggerInstance.getFilePath();
-        if (loggerInstance.getLevel() === Levels.OFF || filePath == null || filePath!.length <= 0){
-            winston.configure({
-                silent : true,
-                transports: [
-                    new winston.transports.Console({
-                        silent: true
-                    })
-                ]
-            });
-        }
-        else if (filePath != null){
-            winston.configure({
-                level: loggerInstance.getLevel(),
-                format: winston.format.combine(
-                    winston.format.timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
-                    winston.format.prettyPrint()
-                ),
-                transports: [
-                    new winston.transports.File({
-                        filename: filePath
-                    })
-                ]
-            });
-        }
-    }
+  private constructor(loggerInstance: Logger) {}
 
-    /**
-     * The method to initialize SDKLogger
-     * @param {Logger} loggerInstance A Logger class instance
-     */
-    public static initialize(loggerInstance: Logger): SDKLogger {
-        return new SDKLogger(loggerInstance);
-    }
+  /**
+   * The method to initialize SDKLogger
+   * @param {Logger} loggerInstance A Logger class instance
+   */
+  public static initialize(loggerInstance: Logger): SDKLogger {
+    return new SDKLogger(loggerInstance);
+  }
 }
